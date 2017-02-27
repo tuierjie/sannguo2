3461d1d63b07ee3d80a7e29ef3faa2946bcc81db
zhouyu <yf17up@126.com>
2017-02-27 20:59:34 +0800

    update 02

--- src/com/sanguo/SanGuo.java
+++ src/com/sanguo/SanGuo.java
@@ -3,6 +3,8 @@ package com.sanguo;
 public class SanGuo {
 	public static void main(String[] args) {
 		System.out.println("hello XiaoQiao");
+		System.out.println("hello XiaoQiao");
+		System.out.println("hello XiaoQiao");
 	}
 
 }
